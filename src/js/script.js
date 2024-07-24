d7f0f81b3d48c14077dd817e7eb1cc73de0a848d
Valentina <mvamks@gmail.com>
2024-07-24 11:49:11 +0300

    portfolio

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,5 @@
+
+
 const gamburger = document.querySelector('.gamburger'),
     menu = document.querySelector('.menu'),   
     close = document.querySelector('.menu__close'); //устанавливаем переменные
@@ -38,13 +40,75 @@ for (i = 0; button.length > i; i++) {
     if (currentActive !== this)
       this.classList.add("active");
   };
-}
+};
+
+
+
+
+
+$(document).ready(function(){
+    $('.contacts__form').validate({
+      rules: {
+        name: {
+          required: true,
+          minlength: 2
+        },
+        email: {
+          required: true,
+          email: true
+        },
+        text: {
+          required: true,
+          maxlength: 1000
+        },
+        policy: {
+          required: true
+        }
+      },
+      messages: {
+        name: {
+            required: "Пожалуйста, введите свое имя",
+            minlength: jQuery.validator.format("Введите {0} символа!")
+          },
+        email: {
+          required: "Пожалуйста, введите свою почту",
+          email: "Неправильно введен адрес почты"
+        },
+        text: "Пожалуйста, введите текст сообщения",
+        policy: "Чтобы продолжить, установите флажок"
+      }
+    });
+
+    $('form').submit(function(e) {
+      e.preventDefault();
+
+      if (!$(this).valid()) {
+        return;
+      }
+
+      $.ajax({
+          type: "POST",
+          url: "mailer/smart.php",
+          data: $(this).serialize()
+      }).done(function() {
+          $(this).find("input").val("");
+          $('form').trigger('reset');
+      });
+      return false;
+  });
+
+});
 
 
 
 
 
 
+  
 
+
+
+  
+   
 
 
