7eca520a08a60d2d8b35435f037e2c946505fd7b
Valentina <mvamks@gmail.com>
2025-11-04 14:25:07 +0200

    fix: исправлены ошибки адаптации

--- src/Pages/Home/home.jsx
+++ src/Pages/Home/home.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Typewriter from 'typewriter-effect';
 import headerBg from "../../assets/img/header2.png";
 import { Link } from "react-router-dom";
@@ -6,14 +7,33 @@ import "./home.scss";
 
 
 const Home = () => {
+    const [loaded, setLoaded] = useState(false);
+    const [error, setError] = useState(false);
     const textLines = [
         "Привет, я Валентина",
         "начинающий Frontend Developer"
     ];
 
+    const changeBg =() => setLoaded(true);
+    const handleError = () => setError(true);
+
     return (
-        <section className="home">
-            <img src={headerBg} alt="header image" />        
+        <section 
+            className="home"
+            style={{
+                backgroundColor: loaded ? 'black' : 'black',
+            }}
+        >
+            {!error && (
+                <img 
+                src={headerBg} 
+                alt="header image"
+                className={`home__bg ${loaded ? "visible" : ""}`}
+                onLoad={changeBg}
+                onError={handleError}
+            />        
+            )}
+            
             <div className="home__content">
                <Typewriter
                     onInit={(typewriter) => {
